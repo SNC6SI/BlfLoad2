411a8d1eb1fb76a47bf262a3a7de325255cf88a7
SNC6SI <shingosch@Hotmail.com>
2020-09-28 14:42:31 +0800

    update author info

--- src/blfc.c
+++ src/blfc.c
@@ -194,7 +194,8 @@ mexFunction(int nlhs,mxArray *plhs[],int nrhs,const mxArray *prhs[])
                 "Input argument must be a string.");
     }
     
-    mexPrintf("\n%s %s\n\n", "   BlfLoad2 by Shen, Chenghao", "<snc6si@gmail.com>.");
+    mexPrintf("\n\t%s %s\n\n",
+             "BlfLoad2 by Shen, Chenghao", "<snc6si@gmail.com>.");
     mexEvalString("drawnow;");
     
     blfInit();
